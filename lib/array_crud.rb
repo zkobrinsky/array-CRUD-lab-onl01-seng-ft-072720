ec0b303c0a6e51dc864483c6360cfbbe1b5438d1
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 17:07:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
