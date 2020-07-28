8ed64c1e88985dae3b16d8493820f8bfa967aadd
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 17:06:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << 5
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(0)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
